27e90eea65bf53fb15c84937f26021e3ed88bfa8
Mihnea Stefan <mihnea.stefan@protonmail.com>
2020-09-19 00:42:11 +0300

    Initial Commit

---
+++ AES/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AES
+//
+//  Created by Mihnea Stefan on 19/09/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
