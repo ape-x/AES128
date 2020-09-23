2f86692aa7b858059a4c6080b2efe56fc4aaf829
Mihnea Stefan <mihnea.stefan@protonmail.com>
2020-09-23 18:20:24 +0300

    Impement Decryption. Current supports single-block inputs.

---
+++ AES/LookUpTables.h
@@ -0,0 +1,22 @@
+//
+//  LookUpTables.h
+//  AES
+//
+//  Created by Mihnea Stefan on 23/09/2020.
+//
+
+#ifndef LookUpTables_h
+#define LookUpTables_h
+
+#include <stdio.h>
+
+extern uint8_t sBox[];
+extern uint8_t inv_sBox[];
+extern uint8_t mul2[];
+extern uint8_t mul3[];
+extern uint8_t mul_9[];
+extern uint8_t mul_11[];
+extern uint8_t mul_13[];
+extern uint8_t mul_14[];
+
+#endif /* LookUpTables_h */
